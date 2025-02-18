ac778ac26e8a4dc6a2d7edfaa658b66f7bad26a8
NiktaP <niktapournabi@gmail.com>
2025-02-18 13:23:22 +0330

    view

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import gray_filter
+from .views import gray_filter,blur_filter
 urlpatterns = [
     path('gray_filter/', gray_filter, name='gray_filter'),
+    path('blur_filter/',blur_filter, name='blur_filter')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
 from django.core.files.storage import default_storage
+from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -14,7 +15,10 @@ def gray(image_path):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return gray_image
 
-
+def blur(image_path):
+    image=cv2.imread(image_path)
+    blurred_image=cv2.blur(image,(15,15))
+    return blurred_image
 @api_view(['POST'])
 def gray_filter(request):
     if 'image' in request.FILES:
@@ -26,10 +30,34 @@ def gray_filter(request):
         converted_img_path=f'{os.path.splitext(image_path)[0]}_gray.jpg'
         cv2.imwrite(converted_img_path,converted_img)
 
-        image_instance = ImagesModels(image=image_file, new_image=image_path)
+        image_instance = ImagesModels(image=image_file, new_image=converted_img_path)
         image_instance.save()
 
         serializer = Imageserializer(image_instance)
 
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        result , buffer = cv2.imencode('.jpg',converted_img)
+
+
+        return HttpResponse(buffer.tobytes(), content_type='image/jpeg')
+    return Response({'status': 'failure'}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def blur_filter(request):
+    if 'image' in request.FILES:
+        image_file=request.FILES['image']
+        image_name=default_storage.save(image_file.name,ContentFile(image_file.read()))
+        image_path=os.path.join(MEDIA_ROOT,image_name)
+
+        converted_img=blur(image_path)
+        converted_img_path=f'{os.path.splitext(image_path)[0]}_blur.jpg'
+        cv2.imwrite(converted_img_path,converted_img)
+
+        image_instance= ImagesModels(image=image_file,new_image=converted_img_path)
+        image_instance.save()
+
+        serializer=Imageserializer(image_instance)
+
+        result, buffer = cv2.imencode('.jpg', converted_img)
+
+        return HttpResponse(buffer.tobytes(), content_type='image/jpeg')
     return Response({'status': 'failure'}, status=status.HTTP_400_BAD_REQUEST)
